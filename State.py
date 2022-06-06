bda33cbcf6818a1cc1ec571560033f3d744c4a03
Stephan <Stephanf2003>
2022-06-07 00:45:57 +0200

    replace add and sub with jump and no jump

--- State.py
+++ State.py
@@ -3,31 +3,45 @@
 class State:
     '''State is used to determine wether to jump from a branch.
 
-        Depending on the correct result the state is changing:
+
+        << State meaining: >>
+        
+        - ``0 or 1:`` No jump is expected for this branch the next time
+        - ``2 or 3:``A jump is expected for this branch the next time
+
+
+
+        << State changing: >>
 
         - ``no jump:`` subtracting one but the lowest is 0
-        - ``Jump:``  adding one but the highest is 11 (Binary)
+        - ``Jump:``  adding one but the highest is 3
+
+        
     '''
 
-    def __init__(self, value = "00") -> None:
+    def __init__(self, value = 0) -> None:
         self.value = value
         self.count = 0              # Number of times a branch is called
         self.correct = 0            # Number of times a branch prediction was correct
 
-    def __add__(self, val):
-        if self.value == "00":      # 00
-            self.value = "01"
-        elif self.value == "01":    # 01
-            self.value = "10"
-        elif self.value == "10":    # 10
-            self.value = "11"
+    def no_jump(self):
+        self.count+=1
 
-    def __sub__(self, val):
-        if self.value == "01":      # 01
-            self.value = "00"
-        elif self.value == "10":    # 10
-            self.value = "01"
-        elif self.value == "11":    # 11
-            self.value = "10"
+        if(self.value in [0, 1]):   # Correct prediction
+            self.value+=1
+            self.correct+=1
+        elif(self.value == 2):      # Wrong prediction
+            self.value+=1
+        elif(self.value == 3):
+            pass
 
+    def jump(self):
+        self.count+=1
 
+        if(self.value == 0):        # Wrong prediction
+            pass
+        elif(self.value == 1):
+            self.value-=1
+        elif(self.value in [2, 3]): # Correct Prediction
+            self.value-=1
+            self.correct+=1
