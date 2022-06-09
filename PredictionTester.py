081a1fb0c745b057c0ab41cdf943e5b26202caa8
Stephan <Stephanf2003>
2022-06-09 16:55:50 +0200

    changed dictonary to tuple

--- PredictionTester.py
+++ PredictionTester.py
@@ -16,7 +16,7 @@ class PredictionTester:
 
     def __init__(self, file_path) -> None:
         self.pht = {}                                           # Pattern history table containing states
-        self.branches = self.__branch_file_to_dic(file_path)
+        self.branches = self.__branch_file_to_list(file_path)   # List of tuples with addresses and jumps
 
         self.count = 0                                          # All branches of last predictor             
         self.correct = 0                                        # Count of branches which have been right predicted
@@ -24,70 +24,75 @@ class PredictionTester:
 
 
 # Predictors        
-    def local_2_bit_predictor(self):                            # TODO: Using async to await the result and show a loading bar in the meantime
+    def local_2_bit_predictor(self, address_size=32):                            # TODO: Using async to await the result and show a loading bar in the meantime
         r'''Test precision of the local 2 Bit predictor
         
-            :param ``address_size``: set Size of Pattern history table keys in bits (e.g. 10 bit)
-        '''   
-
-        for key, jump in self.branches.items():
-            if key not in self.pht:                             # Init branch if not done
-                self.pht[key] = State()
-
-            self.__update_precision(self.pht[key], jump)   
-
-            state = self.pht[key]                               # Set state
-            if jump == '0':                                     # No jump
-                self.pht[key].no_jump()
-            else:                                               # Jump
-                self.pht[key].jump()
-                               
+            :param ``address_size``: set Size of Pattern history table keys in bits (default: 32 bit)
+        ''' 
+        
+        for key, jump in self.branches:
+            address = bin(int(key, 16))[-address_size:]         # address: binary value of old hexadecimal branch address
+            if address not in self.pht:
+                self.pht[address] = State(2)                    # Create new dictonary entry, if key doesn't exist
 
 
+            self.__set_state(address, jump)
+            self.__update_precision(address, jump)  
+
+                                                
+        print(f"Precision rate: {self.precision_rate*100}%")
+        
+        
     async def two_level_global_predictor():
         r'''Test percicion of the two level global predictor'''
 
 
 # Functions
-    def __branch_file_to_dic(self, file_path):
-        r'''Converts branches inside of file into a dictonary
-
-            - Epected format inside file:
-                ``b77b5c36`` ``0`` (branch address, jump)
-            - Use: ``Key:`` branch address, ``Value:`` jump
+    def __branch_file_to_list(self, file_path):
+        r'''Converts branches inside of file into a list of tuples
+               
+                :param ``file_path``: path to a file with the following format (``branch address`` ``jump``)
+            
+            Output in the following format: Tuple(``branch address``, ``jump``)
         '''
 
-        dic = {}                   
+        tem_list = []                               # Temporary list           
         with open(file_path, 'r') as f:
             for b in f.read().splitlines():
-                address, state = b.split(' ')
-                dic[address] = state
+                address, jump = b.split(' ')
+                tem_list.append( (address, jump) )
+        return tem_list
 
-        return dic
+    def __set_state(self, address, jump):
+        r'''Set state depending on jump value
 
-
-    def __hex_to_bin():
-        r'''Convert a hexadecimal number to binary
-        
-            :param ``num``: a hexadecimal number that should be converted
-            :param ``len``: bit length of the binary number (10: takes the 10 least significant bit)
-        
+            :param ``address``: Key for pht to current state
+            :param ``jump``: a integer value of either 0 or 1
         '''
+        
+        state = self.pht[address]
 
-    def __update_precision(self, state, jump):
-        r'''Calculates the precision rate
+        if jump == "0":                 # no Jump
+            state.no_jump()
+        if jump == "1":                 # Jump
+            state.jump()    
+     
+
+    def __update_precision(self, address, jump):
+        r'''Calculates the precision rate depending on expected outcome and actual outcome
             
-            :param ``state:``: Expected prediction for branch
-            :param ``jump:`` Eventaul outcome (jump o. no jump) 
+            :param ``address``: Key for pht to current state
+            :param ``jump``: Eventaul outcome (jump o. no jump) 
         '''
+
+        state = self.pht[address]
+
+        if(state.value in [0, 1] and jump == "0"):                                    # Expected prediction: no jump
+            self.correct+=1
+        elif(state.value in [2, 3] and jump == "1"):                                  # Expected prediction: jump
+            self.correct+=1 
 
         self.count+=1
-        
-        if(state in [0, 1]):                                    # Expected prediction: no jump
-            if jump == 0: self.correct+=1
-        elif(state in [2, 3]):                                  # Expected prediction: jump
-            if jump == 1: self.correct+=1        
-
         self.precision_rate = self.correct / self.count
         
-        print(f"Precision rate: {self.precision_rate}%")
+        
