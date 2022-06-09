846500382887ed80ae3e42370abbb098e20ba832
Stephan <Stephanf2003>
2022-06-09 17:29:03 +0200

    add left shift method

--- State.py
+++ State.py
@@ -1,12 +1,19 @@
-#TODO: implement for diffrent bit
+#TODO: implement for diffrent bit (use bin instead of hex)
 #TODO: e.g. change increment method
 
 class State:
-    '''State is used to determine wether to jump from a branch.
+    '''State is used to predict the next branch (jump or no jump)
 
-        :param ``bit``: the bit each state has
-        :param ``value``: set a start value from 0 to 3
+    Args:
+        :param ``bit``: the size of bit each state has
+        :param ``value``: set a start value from 0 to the hightest possible with n bits
 
+
+    Functions:
+        ????
+
+
+    ---------TODO: redo------
     > State meaining:
         - ``0 or 1:`` No jump is expected for this branch the next time
         - ``2 or 3:``A jump is expected for this branch the next time
@@ -17,7 +24,8 @@ class State:
     '''
 
     def __init__(self, bit, value = 0) -> None:
-        if value >= 0 and value <=3:
+        self.bit = bit                          # Size of history table
+        if value >= 0 and value <=2**bit-1:     # value has to be between 0 and the highest representative number with n bits
             self.value = value
         else:
             self.value = 0
@@ -29,3 +37,11 @@ class State:
     def jump(self):
         if(self.value in [0, 1, 2]):
             self.value+=1
+
+    def left_shift(self, x):
+        r'''Shift the value to the left using x
+        
+            :param ``x``: A bit that is shifted from the right side (0 or 1)
+        '''
+
+        
