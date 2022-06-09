0c5bd8b607b80fe5e2eefc300fbc539794b0de87
Stephan <Stephanf2003>
2022-06-09 10:16:14 +0200

    updated docstring format

--- State.py
+++ State.py
@@ -1,47 +1,29 @@
-#TODO: store precicion rate, when was it right
+#TODO: store precision rate, when was it right
 
 class State:
     '''State is used to determine wether to jump from a branch.
 
+        :param ``value``: set a start value from 0 to 3
 
-        << State meaining: >>
-        
+    > State meaining:
         - ``0 or 1:`` No jump is expected for this branch the next time
         - ``2 or 3:``A jump is expected for this branch the next time
 
-
-
-        << State changing: >>
-
+    > State changing:
         - ``no jump:`` subtracting one but the lowest is 0
         - ``Jump:``  adding one but the highest is 3
-
-        
     '''
 
     def __init__(self, value = 0) -> None:
-        self.value = value
-        self.count = 0              # Number of times a branch is called
-        self.correct = 0            # Number of times a branch prediction was correct
+        if value >= 0 and value <=3:
+            self.value = value
+        else:
+            self.value = 0
 
     def no_jump(self):
-        self.count+=1
-
-        if(self.value in [0, 1]):   # Correct prediction
-            self.value+=1
-            self.correct+=1
-        elif(self.value == 2):      # Wrong prediction
-            self.value+=1
-        elif(self.value == 3):
-            pass
+        if(self.value in [1, 2, 3]):
+            self.value-=1
 
     def jump(self):
-        self.count+=1
-
-        if(self.value == 0):        # Wrong prediction
-            pass
-        elif(self.value == 1):
-            self.value-=1
-        elif(self.value in [2, 3]): # Correct Prediction
-            self.value-=1
-            self.correct+=1
+        if(self.value in [0, 1, 2]):
+            self.value+=1
