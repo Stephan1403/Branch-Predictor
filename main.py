1343b3f5b75bf011b4fe0bd7fde89a0882761692
Stephan <Stephanf2003>
2022-06-14 12:19:22 +0200

    updated testing

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from PredictionTester import PredictionTester
-
 if __name__ == "__main__":
 
-    a = PredictionTester('trace_files/trace.txt')
+    a = PredictionTester('trace_files/trace_gcc.txt')
 
     a.local_2_bit_predictor()
