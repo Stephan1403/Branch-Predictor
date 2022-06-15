cd422b279dace318c4c419ede8ff737a72bcacac
Stephan <Stephanf2003>
2022-06-15 10:34:25 +0200

    updated

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from PredictionTester import PredictionTester
+
 if __name__ == "__main__":
 
-    a = PredictionTester('trace_files/trace.txt')
+    a = PredictionTester('trace_files/trace_gcc.txt')
 
+    
     a.two_level_global_predictor()
