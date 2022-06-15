c580ef0a4717cdbc1923774e79c9b5e9e3c21290
Stephan <Stephanf2003>
2022-06-15 10:33:44 +0200

    added tqdm, fixed two_level_global_predictor

--- PredictionTester.py
+++ PredictionTester.py
@@ -1,5 +1,5 @@
 from State import State
-from tqdm import tqdm
+from tqdm import tqdm                                       # For progress bar
 
 # TODO: change jump (name) to acutal
 
@@ -26,30 +26,32 @@ class PredictionTester:
         self.branches = self.__branch_file_to_list(file_path)   # List of tuples with addresses and actuals
 
         self.count = 0                                          # All branches of last predictor             
-        self.correct = 0                                        # Count of branches which have been right predicted
+        self.correct_predictions = 0                            # Count of branches which have been right predicted
         self.precision_rate = 0                                 # Rate of correct predicted branches
 
 
 # Predictors        
-    def local_2_bit_predictor(self, address_size=32):                            # TODO: Using async to await the result and show a loading bar in the meantime
+    def local_2_bit_predictor(self, address_size=32):
         r'''Test precision of the local 2 Bit predictor.
         
             :param ``address_size``: set Size of Pattern history table keys in bits (default: 32 bit)
         ''' 
-        
-        for key, actual in tqdm(iterable=self.branches, unit='branches' ,colour='green'):
-            address = bin(int(key, 16))[-address_size:]         # address: binary value of old hexadecimal branch address
-            if address not in self.pht:                         # Create new dictonary entry, if key doesn't exist
-                self.pht[address] = State(2)  
 
-            self.__update_precision(address, actual)            # Update Precision first
-            self.__set_state(address, actual)
-                                
+        for key, actual in tqdm(iterable=self.branches, unit="branches" ,colour='green'):       # Iterate through branches, Tqdm is used to show the progress bar
+            address = bin(int(key, 16))[-address_size:]                                         # Address: binary value of hexadecimal branch address
+            if address not in self.pht:                                                         
+                self.pht[address] = State(2)
+
+            state = self.pht[address]                                                           # state is State at the address 'address' in the pattern history table
+            self.__update_precision(state, actual)                                              # First update precision the set state
+            self.__set_state(state, actual)                                   
+             
+
         print(f"Local 2-Bit Predictor\n-{address_size} bit address size\n-------- Precision rate: {self.precision_rate*100}% --------\n")
         
         
         
-    def two_level_global_predictor(self, ghr_size=4):
+    def two_level_global_predictor(self, ghr_size=4):                   #TODO write cleaner
         r'''Test percicion of the two level global predictor
 
             :param ``ghr_size``: size of gloabl history table in bits (default: 4 Bit)
@@ -57,24 +59,98 @@ class PredictionTester:
         Each address in the ghr (global history register) address one cell from the pht (pattern history table)
         '''
 
-        self.ghr = State(ghr_size)                              # default = 4 Bit long global history register
+        self.ghr = State(ghr_size)                                                  # Default = 4 Bit long global history register
 
-        for data in tqdm(iterable=self.branches, unit='branches' ,colour='green'):
-            actual = data[1]                                    # Actual is always the 2nd value of the tuple
-            self.ghr.left_shift(actual)                         # Push Actual value (0 or 1) from the right to change ghr value
+        actuals_list = [data[1] for data in self.branches]
+        for actual in tqdm(iterable=actuals_list, unit="branches" ,colour='green'): # Iterate throug all 'actual' values
             
-            address = self.ghr
+            address = self.ghr.get_val(bin=True)
+            if address not in self.pht:
+                self.pht[address] = State(2)
+
+            state_before_shift = self.pht[address]
+            self.__update_precision(state_before_shift, actual)
+
+            self.ghr.left_shift(actual)                                             # Push Actual value (0 or 1) from the right to change ghr value
+            state_after_shift = self.pht[address]                                              
+            self.__set_state(state_after_shift, actual)
+            
+
+        print(f"2-Lvl-Global-Predictor\n-{ghr_size} global history table size\n-------- Precision rate: {self.precision_rate*100}% --------\n")
+        
+
+
+    def gshar_predictor(self, ghr_size=4):
+        r'''
+
+        '''
+
+        self.ghr = State(ghr_size)
+
+        for key, actual in tqdm(iterable=self.branches, unit="branches", colour='green'):
+            
+
+            self.ghr.left_shift(actual)
+ 
+            address = self.__address_xor_ghr(key)
             if address not in self.pht:
                 self.pht[address] = State(2)
 
             self.__update_precision(address, actual)
             self.__set_state(address, actual)
-            
-        print(f"2-Lvl-Global-Predictor\n-{ghr_size} global history table size\n-------- Precision rate: {self.precision_rate*100}% --------\n")
-        
+
+
+        print(f"G-share-Predictor\n-{ghr_size} global history table size\n-------- Precision rate: {self.precision_rate*100}% --------\n")
 
 
 # Functions
+    def __set_state(self, state, actual):
+        r'''Set state of pattern history table depending on current actual value
+
+            :param ``state``: The state that will be updated
+            :param ``actual``: Actual jump(1) or no jump(0)
+        '''
+
+        if actual == "0":                 # no Jump
+            state.no_jump()
+        if actual == "1":                 # Jump
+            state.jump()    
+     
+
+
+    def __update_precision(self, state, actual):
+        r'''Increase the 
+            
+            :param ``state``: last State before update (what was expected - jump o. no jump)
+            :param ``actual``: Eventual outcome (jump o. no jump) 
+        '''
+
+        state_val = state.get_val()
+
+        #TODO: split jump and no jump depending on size_bit
+        if(state_val in [0, 1] and actual == "0"):                          # Expected prediction: no jump
+            self.correct_predictions+=1
+        elif(state_val in [2, 3] and actual == "1"):                        # Expected prediction: jump
+            self.correct_predictions+=1
+
+        self.count+=1
+        self.precision_rate = self.correct_predictions / self.count
+
+
+
+    def __address_xor_ghr(self, address):
+        r'''XOR a given address with ghr (The length will be the size of ghr)
+
+            :param ``address``: Hexadecimal address
+            :param ``ghr``: binary value of global history table
+        
+        '''
+
+        address_str = str( int(address, 16) )[:-self.ghr.size_bit]                      # Convert and cut address to the correct length
+        return int(address_str) ^ self.ghr.get_val()                                    # Return XOR of address and ghr 
+
+
+
     def __branch_file_to_list(self, file_path):
         r'''Converts branches inside of file into a list of tuples
                
@@ -89,41 +165,3 @@ class PredictionTester:
                 address, actual = b.split(' ')
                 tem_list.append( (address, actual) )
         return tem_list
-
-
-
-    def __set_state(self, address, actual):
-        r'''Set state of pattern history table depending on current actual value
-
-            :param ``address``: pht key to the regarding state
-            :param ``actual``: actual jump(1) or no jump(0)
-        '''
-
-        state = self.pht[address]
-
-        if actual == "0":                 # no Jump
-            state.no_jump()
-        if actual == "1":                 # Jump
-            state.jump()    
-     
-
-
-    def __update_precision(self, address, actual):
-        r'''Calculates the precision rate depending on expected outcome and actual outcome
-            
-            :param ``address``: Key for pht to current state
-            :param ``actual``: Eventual outcome (jump o. no jump) 
-        '''
-
-        state = self.pht[address].get_val()                           # State converted to int value from binary
-
-        #TODO: split jump and no jump depending on size_bit
-        if(state in [0, 1] and actual == "0"):                          # Expected prediction: no jump
-            self.correct+=1
-        elif(state in [2, 3] and actual == "1"):                        # Expected prediction: jump
-            self.correct+=1
-
-        self.count+=1
-        self.precision_rate = self.correct / self.count
-        
-        
