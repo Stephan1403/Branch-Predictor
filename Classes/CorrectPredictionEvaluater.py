e149914e19438d9e4788e6b0c9b315a0b54c81cf
Stephan <Stephanf2003>
2022-06-19 12:59:09 +0200

    created set prediction method

--- Classes/CorrectPredictionEvaluater.py
+++ Classes/CorrectPredictionEvaluater.py
@@ -3,5 +3,20 @@ class CorrectPredictionEvaluater:
         self.count = 0                      # Count all branches
         self.correct_predictions = 0        # Count all predictions who has been as their outcome
 
-    def a(self, ):
-        pass
+    def set_prediction(self, jump_val, actual):
+        r'''Increase the correct_predictions value depending on expected outcome and actual outcome
+            
+        Args:
+            :param ``jump_val``: jump value of state ("no jump" or "jump") - expected outcome
+            :param ``actual``: Eventual outcome (0 or 1)                   - acutal outcome
+        '''
+        self.count+=1
+
+        if(jump_val == "no jump" and actual == "0"):                            # Expected prediction: no jump
+            self.correct_predictions+=1
+            return True
+        elif(jump_val == "jump" and actual == "1"):                             # Expected prediction: jump
+            self.correct_predictions+=1
+            return True
+
+        return False
