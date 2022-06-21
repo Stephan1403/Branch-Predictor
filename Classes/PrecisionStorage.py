d5eea468e393572b5e3d9181bf3b497027a32fd5
Stephan <Stephanf2003>
2022-06-21 17:33:14 +0200

    updated docs

--- Classes/PrecisionStorage.py
+++ Classes/PrecisionStorage.py
@@ -1,20 +1,22 @@
 
 class PrecisionStorage:           # TODO: activate analysis - to store further information
-    r'''Store important information about one Predictor. 
+    r'''Stores important information about a Predictor. 
     
     '''
 
     def __init__(self) -> None:
         self.count = 0                      # Count all branches
-        self.correct_predictions = 0        # Count all predictions who has been as their outcome
+        self.correct_predictions = 0        # Count all predictions which were the same as their outcome
 
 
     def set_precision(self, jump_val, actual):
-        r'''Increase the correct_predictions value depending on expected outcome and actual outcome
+        r'''Increase the correct_predictions value depending on expected outcome and actual outcome.
             
         Args:
             :param ``jump_val``: jump value of state ("no jump" or "jump") - expected outcome
             :param ``actual``: Eventual outcome (0 or 1)                   - acutal outcome
+
+        Return True if prediction was correct otherwise false
         '''
         self.count+=1
 
