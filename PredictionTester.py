cac3a1bc5ac285307b477e2c69fff38e8976c405
Stephan <Stephanf2003>
2022-06-21 17:32:40 +0200

    completed tournament

--- PredictionTester.py
+++ PredictionTester.py
@@ -1,14 +1,16 @@
+from Classes.PatternHistoryTable import PatternHistoryTable
 from Classes.PrecisionStorage import PrecisionStorage
 from Classes.PredictionSelecter import PredictionSelecter
 from Classes.State import State
-from Classes.PatternHistoryTable import PatternHistoryTable
 from tqdm import tqdm                                       # For progress bar
 
 
-# TODO: introduce variable that represents the used predictor (to show what correct_rate is for)
-
 class PredictionTester:
-    r'''Prediction Tester is used to test different branch prediction methods
+    '''
+    Prediction Tester
+    ~~~~~~~~~~~~~~~~~
+    
+    used to test different branch prediction methods
     
     Args:
         :param ``file_path``: path to file containing branches and their actual results
@@ -21,7 +23,8 @@ class PredictionTester:
         - ``tournament_predictor``
 
     Usage:
-        # TODO: ...    
+        >>> pt = PredictionTester('file.txt')  
+        >>> pt.local_2_bit_predictor(state_size=4)
     '''
 
     def __init__(self, file_path) -> None:
@@ -42,17 +45,17 @@ class PredictionTester:
         p_storage = self.percision_storage_dic['local'] = PrecisionStorage()                           # Store all correct predictions
 
         for key, actual in tqdm(iterable=self.branches, unit="branches" ,colour='green'):       # Iterate through branches, Tqdm is used to show the progress bar
-            address = bin(int(key, 16))[-address_size:]                                         # Address: binary value of hexadecimal branch address
+            address = bin( int(key, 16) )[-address_size:]                                         # Address: binary value of hexadecimal branch address
 
-            p_storage.set_precision( pht[address].get_jump_val(), actual )
-            pht[address].set_state(actual)
+            p_storage.set_precision( pht[address].get_jump_val(), actual )                      # Check for correct prediction
+            pht[address].set_state(actual)                                                      # Update State
 
         p_storage.evaluate()
         
         
 
     def two_level_global_predictor(self, ghr_size=4, state_size=2):                   #TODO write cleaner
-        r'''Test percicion of the two level global predictor
+        r'''Test precicion of the two level global predictor
 
         Args
             :param ``ghr_size``: Bit size of the global history register
@@ -66,8 +69,9 @@ class PredictionTester:
         actuals_list = [data[1] for data in self.branches]
         for actual in tqdm(iterable=actuals_list, unit="branches" ,colour='green'):    # Iterate throug all 'actual' values
 
-            # Check for correct prediction
-            address = ghr.get_val( bin=True )   
+            address = ghr.get_val( bin=True ) 
+
+            # Check for correct prediction  
             p_storage.set_precision( pht[address].get_jump_val(), actual)                                             
 
             # Update state for the next ghr value
@@ -92,8 +96,9 @@ class PredictionTester:
 
         for key, actual in tqdm(iterable=self.branches, unit="branches", colour='green'):
 
-            # Check for correct prediction
             address = ghr.xor_address(key)   
+
+            # Check for correct prediction
             p_storage.set_precision( pht[address].get_jump_val(), actual )                
 
             # Set state
@@ -104,73 +109,58 @@ class PredictionTester:
 
 
 
-    #TODO: allow to use gshare instead of just global ???
-    # - alway update ghr, global_pht and local_pht (whatever selected_predictor is)
-    # - but only choosen predictor gives value
-    def tournament_predictor(self, ghr_size=4, state_size=2):
-        r''''''
+    def tournament_predictor(self, ghr_size=4, state_size=2, address_size=32):
+        r'''Test prediction of tournament predictor
 
-        ghr = State(ghr_size)                        
-        pht = PatternHistoryTable(state_size)
-        p_storage = self.percision_storage_dic['global'] = PrecisionStorage()
+        Args:
+            :param ``ghr_size``: Bit size of global history register and theirfore the global pht
+            :param ``state_size``: Bit size of all states in the pattern history tables
+            :param ``address_size``: Bit size of the addresses in the local pattern history table
+        '''
 
-        actuals_list = [data[1] for data in self.branches]
-        for actual in tqdm(iterable=actuals_list, unit="branches" ,colour='green'):    # Iterate throug all 'actual' values
+        p_storage = self.percision_storage_dic['tournament'] = PrecisionStorage()
+        pred_selecter = PredictionSelecter()
 
-            # Check for correct prediction
-            address = ghr.get_val( bin=True )   
-            p_storage.set_precision( pht[address].get_jump_val(), actual)                                             
+        # Global Declaration
+        ghr = State(ghr_size)
+        glo_pht = PatternHistoryTable(state_size)                                   # Global Pattern History Table
 
-            # Update state for the next ghr value
-            pht[address].set_state(actual)
+        # Local Declaration
+        loc_pht = PatternHistoryTable(state_size)                                   # Local Pattern History Table
+
+
+        for key, actual in tqdm(iterable=self.branches, unit="branches" ,colour='green'):
+            
+            address = bin( int(key, 16) )[-address_size:]
+            ghr_val = ghr.get_val( bin=True )
+
+            # Set jump_val_selected pred and second_jump_val depending on the PredictionSelecter
+            if pred_selecter[key] == "global":                                      # Global Predictor selected
+                jump_val_selected_pred = glo_pht[ghr_val].get_jump_val()
+                second_jump_val = loc_pht[address].get_jump_val()
+            else:                                                                   # Local Predictor selected
+                jump_val_selected_pred = loc_pht[address].get_jump_val()
+                second_jump_val = glo_pht[ghr_val].get_jump_val()
+
+
+            # Update precision and store if the prediction was correct in pred_was_correct
+            pred_was_correct = p_storage.set_precision( jump_val_selected_pred, actual)
+
+            # Switch predictors if only the second prediction would have been correct
+            if (not pred_was_correct) and second_jump_val == actual:
+                pred_selecter.switch_predictor_at(key)
+
+
+            # Set global state
+            glo_pht[ghr_val].set_state(actual)
             ghr.left_shift(actual)
+
+            # Set local state
+            loc_pht[address].set_state(actual)
+
 
         p_storage.evaluate()
 
-
-
-        '''
-        selected_predictor = PredictionSelecter()
-        p_storage = self.percision_storage_dic['tournament'] = PrecisionStorage()
-
-        # Global
-        ghr = State(4)
-        global_pht = PatternHistoryTable(2)
-
-        # Local
-        local_pht = PatternHistoryTable(2)
-
-        for key, actual in tqdm(iterable=self.branches, unit="branches", colour='green'):
-            
-            # Global
-            address_global = ghr.get_val( bin=True )
-            global_jump = global_pht[address_global].get_jump_val()
-
-            # Local
-            address_local = bin( int(key, 16) )
-            local_jump = local_pht[address_local].get_jump_val()
-
-            # Set order of jump values depending on selected_predictor
-            selected_jump, other_jump =  self.__set_jumps(selected_predictor[address_local], global_jump, local_jump)
-
-            # Set precision for selected jump and store if the prediction was correct in jump_was_correct
-            jump_was_correct = p_storage.set_precision( selected_jump, actual)
-
-            if not jump_was_correct:
-                if other_jump == actual:                                     # Check if other predictor would have been correct
-                    #self.__switch_local_global( selected_predictor[address_local] )               # Switch from gloabl to lcoal and vice versa
-                    pass
-
-            # Set global state + left shift
-            global_pht[address_global ].set_state(actual)
-            global_pht[address_global].left_shift(actual)
-
-            # Set local state
-            local_pht[address_local].set_state(actual)
-
-        '''
-        #print(p_storage.count, " ", p_storage.correct_predictions)
-        #p_storage.evaluate()
 
 
 # Functions
@@ -189,32 +179,3 @@ class PredictionTester:
                 address, actual = b.split(' ')
                 tem_list.append( (address, actual) )
         return tem_list
-
-
-    def __set_jumps(self, selected_pred, global_jump, local_jump):
-        r'''Return global and local jump in the correct order
-        
-        Args:
-            :param ``selected_pred``: Currently selected predictor (global or local)
-            :param ``global_jump``: Jump value of the global predictor
-            :param ``local_jump`: Jump value of the local predictor
-        '''
-
-        if selected_pred == "global":
-            return global_jump, local_jump
-        else:
-            return local_jump, global_jump
-
-        
-
-    def __switch_local_global(self, selected_pred):
-        r'''Switch a predictor from local to global and vice versa
-
-        Args:
-            :param ``selecteed_pred``: Predictor which contains the value that will be changed
-        '''
-
-        if selected_pred == "global":
-            selected_pred = "local"
-        else:
-            selected_pred = "global"
