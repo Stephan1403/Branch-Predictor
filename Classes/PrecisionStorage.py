3c60b7b1c8ee5f7d7dd87b9b868b4fc2fcc4dee9
Stephan <Stephanf2003>
2022-06-23 12:40:23 +0200

    removed precision rate

--- Classes/PrecisionStorage.py
+++ Classes/PrecisionStorage.py
@@ -9,9 +9,6 @@ class PrecisionStorage:
         self.count = 0                      # Count all branches
         self.correct_predictions = 0        # Count all predictions which were the same as their outcome
 
-        # Lists for graph
-        self.precision_rate_history = [0]
-
     def set_precision(self, jump_val, actual):
         r'''Increase the correct_predictions value depending on expected outcome and actual outcome.
             
@@ -26,20 +23,16 @@ class PrecisionStorage:
 
         if(jump_val == actual):                        # Expected prediction is same as outcome
             self.correct_predictions+=1
-            self.precision_rate_history.append(self.correct_predictions/self.count*100) # For graph
             return True
 
-        self.precision_rate_history.append(self.correct_predictions/self.count*100)     # For graph
         return False
 
 
-    def evaluate(self, predictor_type):
+    def evaluate(self):
         '''Calculate the precision rate for the given predictor
         
         Return history of the precision rate
         '''
-        print(f"Precisionrate = {self.correct_predictions/self.count*100}%")
-
-        return self.precision_rate_history
+        print(f"Precisionrate = {self.correct_predictions/self.count*100}%\n")
 
 
