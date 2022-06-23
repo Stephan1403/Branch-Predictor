6ef80e49bfa673c5e137ede23f6b6cf311145862
Stephan <Stephanf2003>
2022-06-23 12:40:27 +0200

    updated

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ from PredictionTester import PredictionTester
 
 if __name__ == "__main__":
 
-    a = PredictionTester('trace_files/trace.txt')
-    
+    print("\n")
+    a = PredictionTester('trace_files/trace_gcc.txt')
     a.tournament_predictor(address_size=10)
 
 
